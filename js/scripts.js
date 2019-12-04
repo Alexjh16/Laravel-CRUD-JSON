53232648c922910c6012af99ebcef7e08a6038dc
root <root@localhost.localdomain>
2019-12-04 14:30:13 -0500

    fixed

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $("#producto").click(function(){
-        window.location.href="entidad_productos.html";
-    });
-
-    $("#tienda").click(function(){
-        window.location.href="entidad_tienda.html";
-    });
-});
